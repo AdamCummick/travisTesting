b1c42fa1b5845d135e46a60635015d36d12b59e3
AdamCummick <45793216+AdamCummick@users.noreply.github.com>
2019-05-16 11:37:39 -0400

    Update badSketch.ino

--- examples/tempBad/badSketch/badSketch.ino
+++ examples/tempBad/badSketch/badSketch.ino
@@ -4,7 +4,7 @@ void setup() {
   // put your setup code here, to run once:
 
 }
-int i = 0 oops;
+int i = 0; //was an error
 void loop() {
   // put your main code here, to run repeatedly:
 i = incrementValue(i);
